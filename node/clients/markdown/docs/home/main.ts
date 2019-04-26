711f2cdea52ebabd6dc100a41a6ed96b931c66bc
Guilherme Rodrigues <gadr90@gmail.com>
2019-04-26 17:14:12 -0300

    Rename render

--- node/clients/markdown/docs/home/main.ts
+++ node/clients/markdown/docs/home/main.ts
@@ -1,8 +1,8 @@
 export default `
-  # Render Best-Practices
+  # VTEX IO Web Framework Best-Practices
 
   ## Welcome
-  While coding apps, developers face common problems and patterns that are not strictly related to their business logic, like pagination and editing entities. To help you focuss more on your business logic, and less on reinventing the wheel, this repo was created aimming to contain all common patterns, all while respecting **Render's** best practices and performance.
+  While coding apps, developers face common problems and patterns that are not strictly related to their business logic, like pagination and editing entities. To help you focuss more on your business logic, and less on reinventing the wheel, this repo was created aimming to contain all common patterns, all while respecting **VTEX IO Web Framework's** best practices and performance.
 
   ## What can I learn in this code base ?
   In this repo you will learn how to:
@@ -35,13 +35,13 @@ export default `
   An antipattern present in this repo is that the front and back-end code are in the same app. Usually they are split into different apps, given their differences in development cycle. Also, if your project is big enought, it's suggested you split your front-end app into a components app and a **Pages** app (app that places and instanciates these components). For the sake of simplicity and example efficiency, all these apps are joined into this one
 
   ### Basic architecture
-  Since *render-guide* is a VTEX IO app like any other, it's folder structure is well known. In the root folder you can find a \`manifest.json\`, containing all of the app's metadata, like name, vendor, version etc. Also, you will find the following folders, each one matching it's corresponding builder
+  Since *web-framework-guide* is a VTEX IO app like any other, its folder structure is well known. In the root folder you can find a \`manifest.json\`, containing all of the app's metadata, like name, vendor, version etc. Also, you will find the following folders, each one matching it's corresponding builder
 
   - node - *where you actually implement your back-end app*
   - graphql - *where you define your graphql schema*
   - react - *where you write your react components*
   - pages - *where you place and instanciate the components*
 
-  ### what if I didn't understand something ?
+  ### What if I didn't understand something ?
   If you didn't understand something or if you think this doc is incomplete, please open an issue in GitHub!
 `
